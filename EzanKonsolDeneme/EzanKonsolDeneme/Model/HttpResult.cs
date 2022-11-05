634ab1f801aca31fc22746ac3818f29ce17f4985
bekiryildirimm <bekir.yildirim.mdbf21@iste.edu.tr>
2022-11-05 20:20:15 +0300

    Masaüstü ezan vakti Uygulaması için Console üzerinden ezan vakitleri api verileri çekme denemesi. Başarılı.

---
+++ EzanKonsolDeneme/EzanKonsolDeneme/Model/HttpResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Picasso.Model
+{
+    public class HttpResult<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ EzanKonsolDeneme/EzanKonsolDeneme/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Picasso.EzanVakti;
+using Picasso.Services;
+using Picasso;
+
+namespace ezan_vakti_konsol
+{
+
+    class Program
+    {
+        public static string Place(string city)
+        {
+            return $"http://api.aladhan.com/v1/calendarByCity?city={city}&country=Turkey&method=13&month=11&year=2022";
+        }
+        static async Task Main(string[] args)
+        {
+            Console.Write("Sehir Giriniz: ");
+            var city = Console.ReadLine();
+            var prayerTimeApi = Place(city);
+            var httpService = new HttpClientService();
+            var result = await httpService.GetObjectAsync<PrayerTime>(prayerTimeApi);
+
+            if (result.IsSuccess)
+            {
+                var settings = new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                };
+                Console.WriteLine("Tarih    :      Imsak       Gunes       Ogle        Ikindi      Aksam       Yatsi");
+                foreach (var ezan in result.Data.data)
+                {
+                    Console.WriteLine($"{ezan.Date.Gregorian.Date}      {ezan.Timings.Fajr.Remove(5,6)}       {ezan.Timings.Sunrise.Remove(5,6)}       {ezan.Timings.Dhuhr.Remove(5,6)}       {ezan.Timings.Asr.Remove(5,6)}       {ezan.Timings.Sunset.Remove(5, 6)}       {ezan.Timings.Isha.Remove(5,6)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{result.ErrorMessage}");
+            }
+
+        }
+    }
+}
