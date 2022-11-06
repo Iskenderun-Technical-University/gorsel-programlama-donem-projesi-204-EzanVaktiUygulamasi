b8654ab48cd136aaa7a1740acb2616b23ff9dbde
ALİ ASLAN <AL ASLAN@ASLAN-27>
2022-11-06 18:19:46 +0300

    Proje dosyası ekle.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if(__checkbox1_.IsChecked==true)
+            {
+                MessageBox.Show("kutu isaretli..");
+            }
+            else
+            {
+                MessageBox.Show("kutu isaretli degil..");
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ___sehir__SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int indexval = sehir.SelectedIndex;
+            string deger = sehir.Text;
+            ComboBoxItem degerler = (ComboBoxItem)sehir.SelectedItem;
+            string gelendeger = degerler.Content.ToString();
+            MessageBox.Show("Index Deger: " + indexval + "\n" + "Degeri: " + gelendeger);
+        }
+    }
+}
